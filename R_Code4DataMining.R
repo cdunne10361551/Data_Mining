3fcdef38ca6c288f0c96d752cd50be198673f6c0
cdunne10361551 <32909729+cdunne10361551@users.noreply.github.com>
2018-04-04 21:03:23 +0100

    Add files via upload

--- R_Code4DataMining.R
+++ R_Code4DataMining.R
@@ -1,15 +1,18 @@
-##for simplicity and collaboration lets save our dataset found on GitHub to the C:/Temp/HRData##
-##therefore if running this on your individual PC set the following working directory##
+##
+##for simplicity and collaboration lets save our dataset found on GitHub to the C:/Temp/HRData
+##therefore if running this on your individual PC set the following working directory ##
 setwd("C:/Temp/HRData")
 
-##if running this in DBS save the file to your R folder and run the below:##
-#setwd("R:/HRData")
+##
+##if running this in DBS save the file to your R folder and run the below:
+##setwd("R:/HRData") ##
+##
 
 HR <- read.csv("HR_Dataset.csv")
 
-##Splitting the Dataset into Training & Testing##
-##So, we will perform all our analysis on the training set and validate it on our testing set. We shall divide our## 
-##dataset into training(75%) and testing(25%)##
+##Splitting the Dataset into Training & Testing ##
+##So, we will perform all our analysis on the training set and validate it on our testing set. We shall divide our
+##dataset into training(75%) and testing(25%) ##
 install.packages("caret")
 library(caret)
 
@@ -22,4 +25,21 @@ Testing <- HR[-inTrain,]
 library(ggplot2)
 ggplot(Training,aes(Attrition,fill=Attrition))+geom_bar()
 prop.table(table(Training$Attrition))
-##roughly 16% of employees leave the organisation##
+## roughly 16% of employees leave the organisation ##
+
+##Let us look at each variable and see its influence on the Attrition of the organization
+##Age: We see that majority of employees leaving the org are around 30 Years ##
+##Business Travel: Among people who leave, most travel ##
+##Daily Rate: We are not able to see any distinguishable feature here ##
+##Department: Among people attrited employees from HR dept. are less.
+##It is because of low proportion of HR in the organization ##
+
+library(ggplot2)
+library(grid)
+install.packages("gridExtra")
+library(gridExtra)
+agePlot <- ggplot(Training,aes(Age,fill=Attrition))+geom_density()+facet_grid(~Attrition)
+travelPlot <- ggplot(Training,aes(BusinessTravel,fill=Attrition))+geom_bar()
+ratePlot <- ggplot(Training,aes(DailyRate,Attrition))+geom_point(size=4,alpha = 0.05)
+depPlot <- ggplot(Training,aes(Department,fill = Attrition))+geom_bar()
+grid.arrange(agePlot,travelPlot,ratePlot,depPlot,ncol=2,top = "Attrition Information")
